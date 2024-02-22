d12e154a43b3af5405142972700e5a71678e50e8
Neha Kannaujia <Neha.Kannaujia@northumberland.gov.uk>
2024-02-22 17:08:03 +0000

    finished crud functaionality

--- controller/postController.js
+++ controller/postController.js
@@ -1,5 +1,6 @@
-import { fetchApiData, createPost, deleteApiPost } from "../lib/httpClient.js"
+import { fetchApiData, createPost, deleteApiPost, updateApiData} from "../lib/httpClient.js"
 import { logger } from "../middleware/logger.js"
+import { v4 as uuidv4 } from 'uuid';
 
 export const fetchAllPost = async (req, res, next) => {
     try{
@@ -17,7 +18,9 @@ export const addPost = async (req, res, next) => {
     try{
         const data = { title: req.body.title,
         body: req.body.comment,
-        userId: req.body.userid}
+        userId: req.body.userid,
+        id: uuidv4()
+    }
         const results = await createPost("http://localhost:3000/api/getPost", data)
 
         return res.redirect("/")
@@ -46,8 +49,26 @@ export const editPost = async (req, res, next) => {
     try{
         const id = req.params.id
         const results = await fetchApiData(`http://localhost:3000/api/getPost/${id}`)
-        logger.info("postResults" + JSON.stringify(results))
         return res.render("page/updatePost.njk" , { results })
+    }
+    catch(err){
+        logger.error(err);
+        return next(err);
+    }
+}
+
+export const updatePost = async (req, res, next) => {
+    try{
+        const id = req.params.id;
+        const data = { title: req.body.title,
+            body: req.body.comment,
+            userId: req.body.userid,
+            id: req.params.id
+        }
+        logger.info("data" + JSON.stringify(data))
+        const results = await updateApiData(`http://localhost:3000/api/updatePost/${id}`, data)
+        logger.info("update" + JSON.stringify(results))
+        return res.redirect("/")
     }
     catch(err){
         logger.error(err);
--- router/router.js
+++ router/router.js
@@ -1,5 +1,5 @@
 import  express  from "express"
-import { fetchAllPost, addPost, deletePost, editPost} from "../controller/postController.js"
+import { fetchAllPost, addPost, deletePost, editPost, updatePost} from "../controller/postController.js"
 import fakeData from "../data/fakeData.js"
 import { logger } from "../middleware/logger.js";
 const router = express.Router();
@@ -24,6 +24,10 @@ router.get("/editPost/:id", (req, res, next) => {
    return editPost(req,res,next)
 })
 
+router.post("/updatePost/:id", (req, res, next) => {
+   return updatePost(req,res,next)
+})
+
  //Api route to create fetch, create, delete, update data
 
  router.get("/api/getPost", (req, res, next) => {
@@ -37,7 +41,7 @@ router.get("/editPost/:id", (req, res, next) => {
 
 
  router.post("/api/getPost", (req, res, next) => {
-    fakeData.push({...req.body, id: fakeData.length + 1 })
+    fakeData.push({...req.body})
     res.json(fakeData)
  })
 
@@ -45,6 +49,13 @@ router.get("/editPost/:id", (req, res, next) => {
     let newData = fakeData.filter(item => item.id != req.params.id)
     fakeData.splice(0, fakeData.length, ...newData)
     res.json(fakeData)
+})
+
+router.put("/api/updatePost/:id", (req, res, next) => {
+   let newData = fakeData.map(item => item.id == req.params.id ? {...req.body } : item)
+   logger.info("newdata" + JSON.stringify(newData))
+   fakeData.splice(0, fakeData.length, ...newData)
+   res.json(fakeData)
 })
 
  
